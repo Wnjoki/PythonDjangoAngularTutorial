3ff56fc47134aca1393103e8834f8ed2862af401
Teresiah Njoki <njokiwambui02@gmail.com>
2023-04-26 13:23:42 +0200

    Update settings.py

--- DjangoApi/DjangoApi/settings.py
+++ DjangoApi/DjangoApi/settings.py
@@ -23,7 +23,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR,"media")
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j))a)$lw9!23yv8-+-be--mnk@*ely74zs6bq0n$vx2-m(!hp4'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
